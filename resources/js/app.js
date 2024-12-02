816a07ac1d14790de4623063f92e3b63c44e4c9e
raid3n-zero <mehimehilan1111@gmail.com>
2024-12-02 11:36:54 +0530

    🕹️install reverb & vue for frontend

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,8 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './components/ExampleComponent.vue';
+
+const app = createApp(App);
+app.mount('#app');
+// createApp.mount('#app');
